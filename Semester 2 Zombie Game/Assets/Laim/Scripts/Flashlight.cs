2b82196d42161aa56cf5b976dc42b4a3b4025927
liamhenry710 <liamhenry710@gmail.com>
2023-03-15 14:51:23 -0700

    flashlight L

---
+++ Semester 2 Zombie Game/Assets/Laim/Scripts/Flashlight.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private Light flashlight;
+
+    private void Start()
+    {
+        // Find the flashlight component attached to this game object
+        flashlight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        // Check if the F key has been pressed
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Toggle the flashlight on and off
+            flashlight.enabled = !flashlight.enabled;
+        }
+    }
+}
