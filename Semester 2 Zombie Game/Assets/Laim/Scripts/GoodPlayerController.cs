68d17f5edabcf103a9b774d15472f60964c157cd
liamhenry710 <liamhenry710@gmail.com>
2023-04-05 14:55:14 -0700

    redone player controller (I hate you quinten)

---
+++ Semester 2 Zombie Game/Assets/Laim/Scripts/GoodPlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodPlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float lookSensitivity = 2f;
+    public float maxLookUp = 90f;
+    public float maxLookDown = -90f;
+
+    private Rigidbody rb;
+    private float yRotation;
+    private float xRotation;
+    private Camera playerCamera;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCamera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // Movement
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical).normalized * moveSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + transform.TransformDirection(movement));
+
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        // Camera Rotation
+        float mouseX = Input.GetAxis("Mouse X") * lookSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, maxLookDown, maxLookUp);
+
+        transform.eulerAngles = new Vector3(0f, yRotation, 0f);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
+
